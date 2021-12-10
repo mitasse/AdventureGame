8ca0dcfd991cfe167a8a6a3f4df75867491411b2
Maxime Itasse <maxime.itasse@gmail.com>
2021-12-10 21:21:58 +0100

    feat: bow and arrow

---
+++ weapon_behavior/bow_and_arrow_behavior.py
@@ -0,0 +1,9 @@
+"""Bow and arrow behavior"""
+
+
+class BowAndArrowBehavior:
+    """Bow and arrow behavior concrete class"""
+
+    def use_weapon(self) -> None:
+        """Use weapon"""
+        print("Shooting an arrow with a bow")
