e8c59d875c2728440194353374f7657ec0cbc107
Maxime Itasse <maxime.itasse@gmail.com>
2021-12-10 21:21:38 +0100

    feat: knife

---
+++ weapon_behavior/knife_behavior.py
@@ -0,0 +1,9 @@
+"""Knife behavior"""
+
+
+class KnifeBehavior:
+    """Knife behavior concrete class"""
+
+    def use_weapon(self) -> None:
+        """Use weapon"""
+        print("Cutting with a knife")
