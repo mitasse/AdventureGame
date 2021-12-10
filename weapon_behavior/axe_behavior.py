e1836a06131361cd3a0d572edd30aec2846db566
Maxime Itasse <maxime.itasse@gmail.com>
2021-12-10 21:22:06 +0100

    feat: axe

---
+++ weapon_behavior/axe_behavior.py
@@ -0,0 +1,9 @@
+"""Axe behavior"""
+
+
+class AxeBehavior:
+    """Axe behavior concrete class"""
+
+    def use_weapon(self) -> None:
+        """Use weapon"""
+        print("Chopping with an axe")
