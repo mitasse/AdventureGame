5a74e0c812065b584acd041f55986aa501e9f9ca
Maxime Itasse <maxime.itasse@gmail.com>
2021-12-10 21:21:46 +0100

    feat: sword

---
+++ weapon_behavior/sword_behavior.py
@@ -0,0 +1,9 @@
+"""Sword behavior"""
+
+
+class SwordBehavior:
+    """Sword behavior concrete class"""
+
+    def use_weapon(self) -> None:
+        """Use weapon"""
+        print("Swinging a sword")
